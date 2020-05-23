dfd30a9d59b8974a3486a454ae6d0475868dba10
bmulhern2 <bmulhern09@gmail.com>
2020-05-23 08:49:51 -0400

    deploy

--- api-gateway/controllers/authController.js
+++ api-gateway/controllers/authController.js
@@ -1,8 +1,37 @@
-var User = require('./models/user');
+var User = require('../models/user');
+var jwt = require('jsonwebtoken');
+var bcrpyt = require('bcrpytjs');
+var config = require('../config')
 
 exports.user_register = function(req, res) {
-	res.send('NOT IMPLEMENTED: User registration POST');
+	var hashedPassword = bcrypt.hashSync(req.body.password, 8);
+	var newUser = new User({
+		username: req.body.username,
+		password: hashedPassword,
+		email: req.body.email
+	});
+
+	User.add(newUser, (err, user) => {
+		if(err) {
+			return res.status(500).send('There was a problem registering the user.');
+		}
+
+		var token = jwt.sign({ id: user._id}, config.web.secret, {
+			expiresIn: 86400
+		});
+
+		res.statu(200).send({ auth: true, token: token });
+	});
 };
 exports.user_token = function(req, res) {
-	res.send('NOT IMPLEMENTED: User token lookup GET');
+	var token = req.headers['e-access-token'];
+	if (!token) return res.status(401).send({ auth: false, message: 'No token provided'});
+
+	jwt.verify(token, config.web.secret, function(err, decoded) {
+		if(err) return res.status(500).send('There was a problem finding the user');
+		
+		if(!user) return res.status(404).send('No user found');
+
+		res.status(200).send(user);
+	});
 };
--- api-gateway/models/user.js
+++ api-gateway/models/user.js
@@ -5,4 +5,14 @@ var userSchema = new mongoose.Schema({
 	password: String,
 	email: String
 });
+
 module.exports = mongoose.model('User', userSchema);
+
+module.exports.add = (user, callbacck) => {
+	user.save(callback);
+}
+
+module.exports.getById = (id, callback) => {
+	var query = {_id: id};
+	User.findById(query, callback);
+}
